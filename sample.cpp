7a17960262aeab69ae3bfda7f5084e62e27ab632
themastermindnktp <36890561+themastermindnktp@users.noreply.github.com>
2018-09-27 10:00:33 +0700

    Add files via upload

---
+++ sample.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define task "problem"
+#define debug(x) cerr << #x << " = " << x
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    freopen(task".inp", "r", stdin);
+    freopen(task".out", "w", stdout);
+}
